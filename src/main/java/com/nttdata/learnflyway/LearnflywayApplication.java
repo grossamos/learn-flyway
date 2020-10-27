400419e5eeb821ed36fd489b772d3ef74d8b2e36
Amos Groß <amos.gross@nttdata.com>
2020-10-27 11:36:57 +0100

    Added Springboot

---
+++ src/main/java/com/nttdata/learnflyway/LearnflywayApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.learnflyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnflywayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnflywayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/learnflyway/LearnflywayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.learnflyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnflywayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
